520f7e5dd25c8243a85ce08b43c4c88a8c85a661
lampros-m <la.mastorakis@gmail.com>
2025-01-12 17:52:05 +0200

    Introduce async code

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,70 +22,74 @@ const pollingInterval = 3 * 1000;
 // Endpoint const
 const endpoint = 'http://localhost:8090/data';
 
-document.addEventListener("DOMContentLoaded", () => {
-    // Initial fetch
-    fetchDataAndUpdateGrid();
+async function delayAsync(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    // Set interval for polling    
-    refreshContinuously();
-    setInterval(fetchDataAndUpdateGrid, pollingInterval);
+document.addEventListener("DOMContentLoaded", async () => {
+    while (true) {
+        await fetchDataAndUpdateGrid();
+        await delayAsync(pollingInterval);
+    }
 });
 
-const fetchDataAndUpdateGrid = () => {
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-            // Ensure the data is an array and not empty
-            if (!Array.isArray(data) || data.length === 0) {
-                console.error('No data received or data is not an array.');
-                return;
-            }
+async function fetchDataAndUpdateGrid() {
 
-            if (!schema) {
+    try {
+        const response = await fetch(endpoint);
 
-                // Get all existing fields from all rows
-                const newKeys = new Set();
-                data.forEach(item => {
-                    Object.keys(item).forEach(key => {
-                        if (!existingFields.includes(key)) {
-                            newKeys.add(key);
-                        }
-                    });
+        const data = await response.json();
+
+        // Ensure the data is an array and not empty
+        if (!Array.isArray(data) || data.length === 0) {
+            console.error('No data received or data is not an array.');
+            return;
+        }
+
+        if (!schema) {
+
+            // Get all existing fields from all rows
+            const newKeys = new Set();
+            data.forEach(item => {
+                Object.keys(item).forEach(key => {
+                    if (!existingFields.includes(key)) {
+                        newKeys.add(key);
+                    }
                 });
+            });
 
-                // Create additional column definitions for new keys
-                const newColumnDefs = Array.from(newKeys).map(key => ({
-                    field: key,
-                }));
+            // Create additional column definitions for new keys
+            const newColumnDefs = Array.from(newKeys).map(key => ({
+                field: key,
+            }));
 
-                // Sort the columns alphabetically
-                const sortedColumnDefs = newColumnDefs.sort(
-                    (a, b) => a.field.localeCompare(b.field));
+            // Sort the columns alphabetically
+            const sortedColumnDefs = newColumnDefs.sort(
+                (a, b) => a.field.localeCompare(b.field));
 
-                // Update gridOptions with new column definitions
-                gridOptions.columnDefs = sortedColumnDefs;              
-            }
-            else {
+            // Update gridOptions with new column definitions
+            gridOptions.columnDefs = sortedColumnDefs;              
+        }
+        else {
 
-                // Simply use whatever our user gave us without any changes
-                gridOptions.columnDefs = schema.columnDefs;
-            }
+            // Simply use whatever our user gave us without any changes
+            gridOptions.columnDefs = schema.columnDefs;
+        }
 
-            // Update the grid data
-            gridOptions.rowData = data;
+        // Update the grid data
+        gridOptions.rowData = data;
 
-            // Create the grid
-            var gridDiv = document.querySelector("#myGrid");
+        // Create the grid
+        var gridDiv = document.querySelector("#myGrid");
 
-            // If gridApi doesnt exist, create the grid. else update the grid with new data
-            if (gridApi) {
-                gridApi.setGridOption('rowData', data);
-            } else {
-                gridApi = agGrid.createGrid(gridDiv, gridOptions);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-};
-
+        // If gridApi doesnt exist, create the grid. else update the grid with new data
+        if (gridApi) {
+            gridApi.setGridOption('rowData', data);
+        } else {
+            gridApi = agGrid.createGrid(gridDiv, gridOptions);
+        }
+    }
+    catch (error) {    
+        console.error('Error fetching data:', error);
+    }
+}
